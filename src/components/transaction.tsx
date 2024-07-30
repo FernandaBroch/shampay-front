e7c70f6b9dd1ca7c309abdd966180cfe12104da2
Fernanda Broch <fernanda.broch@sap.com>
2024-07-30 00:47:38 -0300

    refactor: handle response

--- src/components/transaction.tsx
+++ src/components/transaction.tsx
@@ -11,19 +11,22 @@ interface TransactionProps {
   sharedAmount: number,
   originalTransactionId: number
 }
-export function Transaction({id, date, description, manualDescription, amount, category, paymentMethod, shared, sharedAmount, originalTransactionId}: TransactionProps) {
+export function Transaction({ id, date, description, manualDescription, amount, category, paymentMethod, shared, sharedAmount, originalTransactionId }: TransactionProps) {
   let button;
   const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState(category);
   const [transaction, setTransaction] = useState({
     id,
+    date,
+    description,
     manualDescription,
-    category
+    category,
+    amount,
+    this: category,
+    paymentMethod,
+    shared,
+    sharedAmount,
+    originalTransactionId
   });
-
-  if(selectedCategory == null){
-    setSelectedCategory(" ")
-  }
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -32,8 +35,8 @@ export function Transaction({id, date, description, manualDescription, amount, c
         let data = await response.json();
         data.unshift(" ")
         setCategories(data);
-        
-      } catch (error) { 
+
+      } catch (error) {
         console.error('Error fetching categories:', error);
       }
     };
@@ -46,82 +49,51 @@ export function Transaction({id, date, description, manualDescription, amount, c
     console.log('Transaction state updated:', transaction);
   }, [transaction]);
 
-  const handleCategoryChange = (event: any) => {
-    setSelectedCategory(event.target.value);
-    setTransaction({ ...transaction, category: event.target.value });
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    const updatedTransaction = { ...transaction, [name]: value };
+    setTransaction(updatedTransaction); 
+    handleResponse(`http://localhost:8080/transactions/${id}`, 'PUT', 'Update transaction', updatedTransaction)
   };
 
   const handleUnshare = async () => {
-    try {
-      const response = await fetch(`http://localhost:8080/transactions/unshared/${id}`, {
-        method: 'PUT',
-      });
-      if (response.ok) {
-        alert('Transaction unshared successfully');
-      } else {
-        alert('Failed to unshare transaction');
-      }
-    } catch (error) {
-      console.error('Error unsharing transaction:', error);
-      alert('An error occurred while unsharing the transaction');
-    }
+    handleResponse(`http://localhost:8080/transactions/unshared/${id}`, 'PUT', 'Unshare Transaction', '')
   }
+  
   const handleShare = async () => {
     const data = {
       originalTransactionId: id,
-      sharedUserId: 2,  
-      duePercentage: 0, 
-      dueAmount: 0      
+      sharedUserId: 2,
+      duePercentage: 0,
+      dueAmount: 0
     };
+    handleResponse(`http://localhost:8080/transactions/shared/${id}`, 'PUT', 'Share transaction', data)
+    
+  };
 
+  const handleResponse = async (url: string, method: string, actionName: string, data: any) => {
     try {
-      const response = await fetch(`http://localhost:8080/transactions/shared/${id}`, {
-        method: 'PUT',
+      const response = await fetch(url, {
+        method: method,
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
       });
       if (response.ok) {
-        alert('Transaction shared successfully');
+        console.log(actionName + ' successfully');
       } else {
-        alert('Failed to share transaction');
+        alert(actionName + 'failed');
       }
     } catch (error) {
-      console.error('Error sharing transaction:', error);
-      alert('An error occurred while sharing the transaction');
+      console.error(actionName + 'Error', error);
+      alert(actionName + 'error occurred');
     }
-  };
-  const handleUpdate = async () => {
-    console.log(transaction)
-    try {
-      const response = await fetch(`http://localhost:8080/transactions/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(transaction),
-      });
-      if (response.ok) {
-        alert('Transaction updated successfully');
-      } else {
-        alert('Failed to update transaction');
-      }
-    } catch (error) {
-      console.error('Error updating transaction:', error);
-      alert('An error occurred while updating the transaction');
-    }
-  };
+  }
 
-  const handleChange = (e: any) => {
-    const { name, value } = e.target;
-    setTransaction({ ...transaction, [name]: value });
-    console.log({transaction})
-  };
-
-  if (originalTransactionId == id ) {
+  if (originalTransactionId == id) {
     button = <a onClick={handleUnshare} className="waves-effect waves-light red lighten-2 btn">Unshare</a>;
-  } else if(originalTransactionId == null || originalTransactionId == 0) {
+  } else if (originalTransactionId == null || originalTransactionId == 0) {
     button = <a onClick={handleShare} className="waves-effect waves-light lighten-2 btn">Share</a>;
   }
 
@@ -133,20 +105,19 @@ export function Transaction({id, date, description, manualDescription, amount, c
       <td><input type="text" name="manualDescription" value={transaction.manualDescription} onChange={handleChange} /></td>
       <td>{amount}</td>
       <td className='categorySelect'>
-        <select title={category} name="category" value={selectedCategory} onChange={handleCategoryChange} className="browser-default">
+        <select title={category} name="category" value={transaction.category} onChange={handleChange} className="browser-default">
           {categories.map((category) => (
             <option key={category} value={category}>
               {category}
             </option>
           ))}
         </select>
-        </td>
+      </td>
       <td>{paymentMethod}</td>
       <td>{originalTransactionId}</td>
       <td>{shared}</td>
       <td>{sharedAmount}</td>
       <td>{button}</td>
-      <td><button className="waves-effect waves-light yellow darken-2 btn" onClick={handleUpdate}>Update</button></td>
     </tr>
   )
 }
